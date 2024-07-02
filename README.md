90c6653fa80faaf1a1f8a343a98d87b34f7a5c9a
POFFIOLEG <haw9kbtw@gmail.com>
2024-07-02 12:17:58 +0300

    32ке

---
+++ README.md
@@ -0,0 +1,1 @@
+# web2_3.github.io
---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+require_once ('global.php');
+$login = $_POST['login'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$date = $_POST['date'];
+$bio = $_POST['bio'];
+?>
